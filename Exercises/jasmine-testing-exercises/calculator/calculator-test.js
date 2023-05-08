df73a02e36224b623e4db5463f7e72b3c108f92a
arieltaveras <ariel.taveras7278@gmail.com>
2023-05-08 13:07:01 -0400

    completed functions to make calculator work and made tests to run

--- Exercises/jasmine-testing-exercises/calculator/calculator-test.js
+++ Exercises/jasmine-testing-exercises/calculator/calculator-test.js
@@ -1,11 +1,40 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  const values = {
+    amount: 10000,
+    years: 10,
+    rate: 10
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('132.15')
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {
+    amount: 10064,
+    years: 10,
+    rate: 10
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('133.00')
 });
 
+it('should handle a very long term in years', ()=>{
+  const values = {
+    amount: 10000,
+    years: 100,
+    rate: 10
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('83.34')
+});
+
+it('should handle a 2 decimal rate', ()=>{
+  const values = {
+    amount: 10000,
+    years: 100,
+    rate: 10.68
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('89.00')
+});
 /// etc
